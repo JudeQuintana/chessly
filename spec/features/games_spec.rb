0d75d0ac036815bacaede5803b36a74155a9a569
Jude Quintana <jude.quintana@gmail.com>
2015-09-13 18:17:58 -0600

    styled game index, cleaned out games controller, changed game spec, added nav in layout

--- spec/features/games_spec.rb
+++ spec/features/games_spec.rb
@@ -1,12 +1,21 @@
 require "rails_helper"
 
-RSpec.describe "Game Index" do
-  it "shows a list of games" do
-    create(:game)
-
+RSpec.describe "Game" do
+  it "shows an empty list of games" do
     visit root_path
 
     expect(page).to have_content("Game List")
-
+    expect(page).to have_content("No games, try creating one!")
+    expect(page).to have_content("White")
+    expect(page).to have_content("Black")
+    expect(page).to have_content("Result")
   end
+
+  xit "shows a list of existing games" do
+  end
+
+  def create_game
+    create(:game)
+  end
+
 end
