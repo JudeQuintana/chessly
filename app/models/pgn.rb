20fd4b18dfeff7956465fb14ddb8c6bae1f1e7b7
Jude Quintana <jude.quintana@gmail.com>
2015-09-22 19:04:58 -0600

    refactored pgn validations

--- app/models/pgn.rb
+++ app/models/pgn.rb
@@ -4,64 +4,68 @@ class Pgn
   attr_accessor :text
 
   validates :text, :presence => true
-  validate :check_pgn!, :if => :text_present?
+  validate :check_format!
 
-  def initialize(attrs = {}, pgn_parser = PGN)
-    self.pgn_parser = pgn_parser
+  def initialize(attrs = {}, parser = PGN)
+    self.parser = parser
     super(attrs)
   end
 
   def save
     return false unless valid?
-    game = Game.new(game_params)
-    if game.save
-      true
-    else
-      add_errors_from_game!(game)
-      false
-    end
+    Game.create!(game_params)
+    true
   end
+
 
   private
 
-  attr_accessor :pgn_parser, :game_params
+  attr_accessor :parser, :game_params, :pgn_game
 
-  def check_pgn!
+  def check_format!
+    return if !text.present?
+
     begin
-      pgn_text = clean!
-      pgn_game = pgn_parser.parse(pgn_text).first
+      clean!
+      self.pgn_game = parser.parse(text).first
     rescue
       could_not_be_parsed!
       return
     end
 
-    build_game_params!(pgn_game, pgn_text)
+    build_game_params!
+    check_game_params!
   end
 
   def clean!
-    pgn_text = text.dup
-    pgn_text.strip! #remove carriage return
-    pgn_text.gsub(/[\u201c|\u201d]/, "\"") #replace unicode quotation marks
-    pgn_text.gsub!(/\$\d+\s/, "") #removes NAG
-    pgn_text.gsub!(/\n/, " ") #replace new line with space
-    pgn_text
+    text.strip! #remove carriage return
+    text.gsub!(/[\u201c|\u201d]/, "\"") #replace unicode quotation marks
+    text.gsub!(/\$\d+\s/, "") #removes NAG
+    text.gsub!(/\n\s/, " ") #replace new line with space
   end
 
-  def build_game_params!(pgn_game, pgn_text)
+  def build_game_params!
     self.game_params = { :white  => pgn_game.tags["White"],
                          :black  => pgn_game.tags["Black"],
                          :result => pgn_game.tags["Result"],
-                         :pgn    => pgn_text }
+                         :pgn    => text }
   end
 
-  def add_errors_from_game!(game)
-    game.errors.messages.each do |err_key, err_msg|
-      errors.add(err_key, err_msg.first)
+  def check_game_params!
+    game = new_game(game_params)
+    if !game.valid?
+      add_errors_from_game(game)
     end
   end
 
-  def text_present?
-    text.present?
+  def new_game(game_params)
+    Game.new(game_params)
+  end
+
+  def add_errors_from_game(game)
+    game.errors.messages.each do |err_key, err_msg|
+      errors.add(err_key, err_msg.first)
+    end
   end
 
   def could_not_be_parsed!
